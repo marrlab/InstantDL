71006d0ce62e6cde39fde92c53acebbe3889c0d4
Ali Boushehri <ali.shetab.boushehri@gmail.com>
2020-05-18 11:43:49 +0200

    fixed problem with the docker bin bash

--- docker/bin/docker_build_context.sh
+++ docker/bin/docker_build_context.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
+DIR=$(pwd)
 
-rm -rf ${DIR}/docker/build
+rm -rf ${DIR}/build
 
-mkdir -p ${DIR}/docker/build
-mkdir -p ${DIR}/docker/build/classification
-mkdir -p ${DIR}/docker/build/data_generator
-mkdir -p ${DIR}/docker/build/segmentation
-mkdir -p ${DIR}/docker/build/evaluation
+mkdir -p ${DIR}/build
+mkdir -p ${DIR}/build/classification
+mkdir -p ${DIR}/build/data_generator
+mkdir -p ${DIR}/build/segmentation
+mkdir -p ${DIR}/build/evaluation
 
-cp ${DIR}/docker/Dockerfile ${DIR}/docker/build
-cp ${DIR}/instantdl/*.py ${DIR}/docker/build
-cp ${DIR}/instantdl/config.json ${DIR}/docker/build # you need to pass the right config file
+cp ${DIR}/docker/Dockerfile ${DIR}/build
+cp ${DIR}/instantdl/*.py ${DIR}/build
+cp ${DIR}/instantdl/config.json ${DIR}/build # you need to pass the right config file
 
-cp ${DIR}/instantdl/classification/*.py ${DIR}/docker/build/classification
-cp ${DIR}/instantdl/data_generator/*.py ${DIR}/docker/build/data_generator
-cp ${DIR}/instantdl/segmentation/*.py ${DIR}/docker/build/segmentation
-cp ${DIR}/instantdl/evaluation/*.py ${DIR}/docker/build/evaluation
+cp ${DIR}/instantdl/classification/*.py ${DIR}/build/classification
+cp ${DIR}/instantdl/data_generator/*.py ${DIR}/build/data_generator
+cp ${DIR}/instantdl/segmentation/*.py ${DIR}/build/segmentation
+cp ${DIR}/instantdl/evaluation/*.py ${DIR}/build/evaluation
