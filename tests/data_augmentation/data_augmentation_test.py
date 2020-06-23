b595d98531382b5ea1322f11d8b771ff647c9416
dominik.waibel <dominik.waibel@helmholtz-muenchen.de>
2020-06-23 16:36:19 +0200

    extended data augmentation test

--- tests/data_augmentation/data_augmentation_test.py
+++ tests/data_augmentation/data_augmentation_test.py
@@ -11,17 +11,37 @@ def test_data_augentation():
                     "feature_scaling": True,
                     "horizontal_flip": True,
                     "vertical_flip": True,
-                    "poission_noise": True,
-                    "rotation_range": True,
-                    "zoom_range": True,
-                    "contrast_range": True,
-                    "brightness_range": True,
-                    "gamma_shift": True,
+                    "poission_noise": 0.2,
+                    "rotation_range": 10,
+                    "zoom_range": 2,
+                    "contrast_range": 2,
+                    "brightness_range": 2,
+                    "gamma_shift": 1,
                     "threshold_background_image": True,
                     "threshold_background_groundtruth": True,
                     "binarize_mask": True
     }
     data_path_file_name = "Random"
     assert np.shape(data_augentation(X, X, data_gen_args, data_path_file_name)[0]) == shape
-    assert np.max(data_augentation(X, X, data_gen_args, data_path_file_name)[0]) == 0.0
+    assert np.max(data_augentation(X, X, data_gen_args, data_path_file_name)[0]) == 0.
     assert np.min(data_augentation(X, X, data_gen_args, data_path_file_name)[0]) == 0.0
+    X[10:50, 10:50, :] = 255.
+    data_gen_args = {"save_augmented_images": False,
+                     "resample_images": False,
+                     "std_normalization": False,
+                     "feature_scaling": False,
+                     "horizontal_flip": True,
+                     "vertical_flip": True,
+                     "poission_noise": False,
+                     "rotation_range": False,
+                     "zoom_range": 1.5,
+                     "contrast_range": False,
+                     "brightness_range": False,
+                     "gamma_shift": False,
+                     "threshold_background_image": False,
+                     "threshold_background_groundtruth": False,
+                     "binarize_mask": False
+                     }
+    assert np.shape(data_augentation(X, X, data_gen_args, data_path_file_name)[0]) == shape
+    assert np.max(data_augentation(X, X, data_gen_args, data_path_file_name)[0]) == 255.
+    assert np.min(data_augentation(X, X, data_gen_args, data_path_file_name)[0]) == 0.
