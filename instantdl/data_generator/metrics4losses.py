91e7ee689e99ef91fda4111b41370ab2982774f2
dominik.waibel <dominik.waibel@helmholtz-muenchen.de>
2021-03-29 10:55:17 +0200

    added dice loss

--- instantdl/data_generator/metrics4losses.py
+++ instantdl/data_generator/metrics4losses.py
@@ -1,1 +1,15 @@
-# In this file loss function, which are not in keras are implemented
+# File for custom loss functions
+
+from keras import backend as K
+
+def dice_coef(y_true, y_pred, smooth=1):
+    """
+    Dice = (2*|X & Y|)/ (|X|+ |Y|)
+         =  2*sum(|A*B|)/(sum(A^2)+sum(B^2))
+    ref: https://arxiv.org/pdf/1606.04797v1.pdf
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
+
+def dice_loss(y_true, y_pred):
+    return 1-dice_coef(y_true, y_pred)# In this file loss function, which are not in keras are implemented
